df92a2f1c762bfff329efd427980b2cc17f518aa
Eric Lawless <elawless@netflix.com>
2014-02-23 01:11:19 -0800

    Point to Jafar, add example (throw doesn't work yet).

---
+++ example.js
@@ -0,0 +1,34 @@
+var reader = {
+    value: 1,
+    read: function() {
+        if (this.value > 20) {
+            return { done: true };
+        }
+        var promise = new $__Promise__();
+        var value = this.value++;
+        setTimeout(function() {
+            promise.fulfill({ done: false, value: value });
+        }, 100);
+        return promise;
+    }
+}
+
+async function *test() {
+    while (true) {
+        var result = await reader.read();
+        if (result.done) return;
+        if (result.value === 15) {
+            throw new Error(result.value);
+        }
+        yield result.value;
+    }
+}
+
+test({
+    next: function(value) {
+        console.log(value);
+    },
+    throw: function(error) {
+        console.error(error);
+    }
+});
